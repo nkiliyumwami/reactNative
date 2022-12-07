fec85973740cd8159a900f5dc0d847ccb52a7af4
Emmanuel Nkiriyumwami <nkiliyumwami@gmail.com>
2022-12-07 18:46:23 +0200

    Post listing to a server

--- app/api/listings.js
+++ app/api/listings.js
@@ -1,10 +1,34 @@
-import client from './client'
+import { DatePickerAndroid } from 'react-native';
+import client from './client';
 
-const endpoint = '/listings'
+const endpoint = '/listings';
 
-const getListings = () => client.get(endpoint)
+//Get a list of all the listings
+const getListings = () => client.get(endpoint);
+
+//Post a listing to the server
+const addListing = (listing) => {
+  const data = new FormData();
+  data.append('title', listing.title);
+  data.append('price', listing.price);
+  data.append('categoryId', listing.category.value);
+  data.append('description', listing.description);
+
+  listing.images.forEach((image, index) =>
+    data.append('image', {
+      name: 'image' + index,
+      type: 'image/jpeg',
+      uri: image,
+    })
+  );
+
+  if (listing.location)
+    data.append('location', JSON.stringify(listing.location));
+
+  return client.post(endpoint, data);
+};
 
 export default {
-    getListings,
-}
-
+  addListing,
+  getListings,
+};
